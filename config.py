d34549bea5d1fe68828f51b3fc950301feaf54af
kev127 <kelvinkeya125@gmail.com>
2020-11-02 08:47:30 +0300

    add README.md and secret key,quote url

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+
+class Config:
+
+    class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    QUOTE_URL ='http://quotes.stormconsultancy.co.uk/quotes.json'
+
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
