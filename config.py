9d42a8a2a5ddc0970795c8184ffd56a2ec5af78e
kev127 <kelvinkeya125@gmail.com>
2020-11-02 12:30:42 +0300

    add SQLALCHEMY_DATABASE

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ class Config:
     class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     QUOTE_URL ='http://quotes.stormconsultancy.co.uk/quotes.json'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kev:@Y0a!yAV@localhost/blog'
 
 
 class ProdConfig(Config):
